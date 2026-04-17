5fc84cf4cb320e90213ac0f575fe33c7d24af44d
Matheus Reinaldo <reinaldomatheusdeveloper@gmail.com>
2026-04-17 12:31:47 -0300

    refactor: loginComUsuario no fluxo 3

--- cypress/e2e/03-agendamento.cy.js
+++ cypress/e2e/03-agendamento.cy.js
@@ -1,19 +1,6 @@
 describe("Fluxo 3 - Agendamento de Consulta Presencial", () => {
   beforeEach(() => {
-    cy.fixture("usuario").then((user) => {
-      cy.intercept("POST", "**/auth/login**").as("loginReq");
-
-      cy.visit("/");
-      cy.get('[data-cy="btn-login"]').click();
-      cy.get('[data-cy="form-login"]').should("be.visible");
-      cy.get("#user").type(user.email);
-      cy.get("#password").type(user.senha);
-      cy.get('[data-cy="checkbox-aceita-politicas"]')
-        .find("input")
-        .check({ force: true });
-      cy.get('[data-cy="btn-submit-login"]').click();
-      cy.wait("@loginReq").its("response.statusCode").should("eq", 200);
-    });
+    cy.loginComUsuario();
   });
 
   it("deve agendar uma consulta presencial com sucesso", () => {
